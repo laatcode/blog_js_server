6daf7a55cce2e2ef58734cb4f45ddd5985d4364b
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2022-05-08 17:48:23 -0500

    Initial basic structure

---
+++ src/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => res.send('Server running'))
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
